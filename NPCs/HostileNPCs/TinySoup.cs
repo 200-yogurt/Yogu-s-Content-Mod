07492e001db04098e618c617e6edb35ed4a305c0
the forgotten yogurt <65128443+200-yogurt@users.noreply.github.com>
2021-09-30 17:40:05 -0500

    TinySoup little change

--- NPCs/HostileNPCs/TinySoup.cs
+++ NPCs/HostileNPCs/TinySoup.cs
@@ -7,11 +7,12 @@ namespace YoguContentMod.NPCs.HostileNPCs
 {
     public class TinySoup : ModNPC
     {
-        public override void SetStaticDefaults()
+        public override void SetStaticDefaults() 
         {
             Main.npcFrameCount[npc.type] = 4;
         }
-        public override void SetDefaults()
+
+        public override void SetDefaults() 
         {
             npc.width = 24;
             npc.height = 34;
@@ -20,21 +21,20 @@ namespace YoguContentMod.NPCs.HostileNPCs
             npc.defense = 2;
             npc.lifeMax = 25;
             npc.HitSound = SoundID.NPCHit1;
-            npc.DeathSound = SoundID.NPCDeath1;
+            npc.DeathSound = SoundID.NPCDeath1; 
             npc.value = 25f;
-            SetBuffImmune(20, 30, 70, 31, 149);
+            npc.buffImmune[20] = true;
+            npc.buffImmune[30] = true;
+            npc.buffImmune[70] = true;
+            npc.buffImmune[31] = true;
+            npc.buffImmune[149] = true;
         }
-        void SetBuffImmune(params int[] BuffTypes)
-        {
-            foreach (int buffType in BuffTypes)
-            {
-                npc.buffImmune[buffType] = true;
-            }
-        }
+
         public override float SpawnChance(NPCSpawnInfo spawnInfo)
         {
             return SpawnCondition.OverworldDay.Chance * 0.55f;
         }
+
         private const int AI_State_Slot = 0;
         private const int AI_Timer_Slot = 1;
         private const int AI_TinySoup_Slot = 2;
@@ -105,6 +105,7 @@ namespace YoguContentMod.NPCs.HostileNPCs
                     }
                 }
             }
+
             else if (AI_State == State_Float)
             {
                 npc.TargetClosest(true);
@@ -116,13 +117,13 @@ namespace YoguContentMod.NPCs.HostileNPCs
                     AI_State = State_Attack;
                 }
             }
+
             else if (AI_State == State_Attack)
             {
                 npc.TargetClosest(true);
                 Player target = Main.player[npc.target];
                 Vector2 directionToPlayer = npc.DirectionTo(target.Center);
                 npc.velocity = Vector2.Lerp(npc.velocity, directionToPlayer * 1f, 0.1f);
-
             }
         }
     }
