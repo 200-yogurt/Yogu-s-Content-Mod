c6fcc3453047e6c5014fdb047e081af4dd1538fa
the forgotten yogurt <65128443+200-yogurt@users.noreply.github.com>
2021-09-30 17:38:58 -0500

    Some progress on boss

--- NPCs/BossNPCs/YoguBoss/YoguBoss.cs
+++ NPCs/BossNPCs/YoguBoss/YoguBoss.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader; 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using YoguContentMod.NPCs.HostileNPCs;
 
 namespace YoguContentMod.NPCs.BossNPCs.YoguBoss
 {
@@ -11,17 +12,31 @@ namespace YoguContentMod.NPCs.BossNPCs.YoguBoss
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("King Yogurt");
+            DisplayName.SetDefault("King Yoqurt"); // its pronounced yogurt but its written as "Yoqu"
+            Main.npcFrameCount[npc.type] = 8;
         }
 
         public override void SetDefaults()
         {
-
+            npc.lifeMax = 3_900; 
+            npc.width = 130;
+            npc.height = 134;
+            npc.damage = 31; 
+            npc.boss = true; 
+            npc.aiStyle = -1; 
+            npc.value = Item.sellPrice(gold: 13, silver: 50); 
         }
+
+        public override void ScaleExpertStats(int numPlayers, float bossLifeScale) 
+        { 
+            npc.lifeMax = 6_780;
+            npc.damage = 62;
+            npc.value = Item.sellPrice(gold: 18, silver: 80);
+        } 
 
         const int StateSlot = 0;
         const int TimerSlot = 1;
-        const int SecondaryTimerSlot =21;
+        const int SecondaryTimerSlot = 2;
 
         AIState State { get => (AIState)(int)npc.ai[StateSlot]; set => npc.ai[StateSlot] = (int)value; }
         float Timer { get => npc.ai[TimerSlot]; set => npc.ai[TimerSlot] = value; }
@@ -48,6 +63,7 @@ namespace YoguContentMod.NPCs.BossNPCs.YoguBoss
                     jumpCount = 0;
                 }
             }
+
             else if(State == AIState.LaserCharge)
             {
                 if(Timer < LaserChargeTime){
@@ -60,6 +76,7 @@ namespace YoguContentMod.NPCs.BossNPCs.YoguBoss
                     Timer = 0;
                 } 
             } 
+
             else if(State == AIState.LaserAttack)
             {
                 if(Timer < LaserHoldTime) 
@@ -83,23 +100,33 @@ namespace YoguContentMod.NPCs.BossNPCs.YoguBoss
                     }
                 }
             }
-            else if(State == AIState.RandomProjectileShooting)
-            {
-                if(Timer < 20 && npc.life >= npc.lifeMax * 0.4f) // if npc is behind 40% projectile spam, otherwise, every 0.3 secs (20 ticks)
-                {
-                    Timer++; 
-                }
-                else // spawn projectile
-                {
-                    float projectileShootSpeed = 20;
-                    Vector2 direction = Vector2.UnitY.RotatedByRandom(28159f) * projectileShootSpeed;
 
-                    Projectile proj = Projectile.NewProjectileDirect(npc.Center, direction, ProjectileID.MeteorShot, 9, 1f, Main.myPlayer); 
-                    proj.friendly = false; 
-                    proj.hostile = true;
+            else if(State == AIState.RandomProjectileShooting) //keep shooting
+            {
+                if (Timer < RandomProjectileShootingTime)
+                {
+                    Timer++;
+                    if (Timer % 20 < 1 || npc.life < npc.lifeMax * 0.4f) // if npc is behind 40% projectile spam, otherwise, every 0.3 secs (20 ticks)
+                    {
+                        // spawn projectile
+                        float projectileShootSpeed = 20;
+                        Vector2 direction = Vector2.UnitY.RotatedByRandom(28159f) * projectileShootSpeed;
+
+                        Projectile proj = Projectile.NewProjectileDirect(npc.Center, direction, ProjectileID.MeteorShot, 15, 1f, Main.myPlayer);
+                        proj.friendly = false;
+                        proj.hostile = true;
+                    }
+                }
+                else // shooting stops
+                {
+                    Timer = 0;
+                    State = AIState.NPCJumping; 
+                    SpawnMiniSoups();        
                 }
             }    
         }
+
+        int RandomProjectileShootingTime => 120;
 
         int laserShootCount;
         float LaserHoldTime = 60;
@@ -115,6 +142,13 @@ namespace YoguContentMod.NPCs.BossNPCs.YoguBoss
             LaserCharge,
             LaserAttack,
             RandomProjectileShooting,
+        }
+
+        void SpawnMiniSoups()
+        {
+            Vector2 off = new Vector2(0, 100);
+            Vector2 center = npc.Center;
+            NPC.NewNPC((int)(center.X + off.X), (int)(center.Y + off.Y), ModContent.NPCType<TinySoup>());
         }
         
         void Jump()
