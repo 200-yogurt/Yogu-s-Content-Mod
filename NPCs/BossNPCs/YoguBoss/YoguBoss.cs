a9357d244b2debe38b985aae27a6bbb6999a396c
the forgotten yogurt <65128443+200-yogurt@users.noreply.github.com>
2021-10-01 18:24:14 -0500

    lolxd conchetumare

--- NPCs/BossNPCs/YoguBoss/YoguBoss.cs
+++ NPCs/BossNPCs/YoguBoss/YoguBoss.cs
@@ -12,26 +12,27 @@ namespace YoguContentMod.NPCs.BossNPCs.YoguBoss
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("King Yoqurt"); // its pronounced yogurt but its written as "Yoqu"
+            DisplayName.SetDefault("King Yoqurt"); // its pronounced yogurt but its written as "Yoqurt"
             Main.npcFrameCount[npc.type] = 8;
         }
 
         public override void SetDefaults()
         {
             npc.lifeMax = 3_900; 
-            npc.width = 130;
+            npc.width = 130; 
             npc.height = 134;
             npc.damage = 31; 
             npc.boss = true; 
-            npc.aiStyle = -1; 
-            npc.value = Item.sellPrice(gold: 13, silver: 50); 
+            npc.aiStyle = -1;  
+            npc.noGravity = false; 
+            npc.value = Item.sellPrice(gold: 13, silver: 50);  
         }
 
         public override void ScaleExpertStats(int numPlayers, float bossLifeScale) 
-        { 
+        { // a
             npc.lifeMax = 6_780;
-            npc.damage = 62;
-            npc.value = Item.sellPrice(gold: 18, silver: 80);
+            npc.damage = 62; // 
+            npc.value = Item.sellPrice(gold: 18, silver: 80); ;
         } 
 
         const int StateSlot = 0;
@@ -39,9 +40,11 @@ namespace YoguContentMod.NPCs.BossNPCs.YoguBoss
         const int SecondaryTimerSlot = 2;
 
         AIState State { get => (AIState)(int)npc.ai[StateSlot]; set => npc.ai[StateSlot] = (int)value; }
-        float Timer { get => npc.ai[TimerSlot]; set => npc.ai[TimerSlot] = value; }
-        float SecondaryTimer { get => npc.ai[SecondaryTimerSlot]; set => npc.ai[SecondaryTimerSlot] = value; }
+
+        ref float Timer => ref npc.ai[TimerSlot]; //{ get => npc.ai[TimerSlot]; set => npc.ai[TimerSlot] = value; }
+        ref float SecondaryTimer => ref npc.ai[SecondaryTimerSlot]; //{ get => npc.ai[SecondaryTimerSlot]; set => npc.ai[SecondaryTimerSlot] = value; }
         Player Target { get => Main.player[npc.target]; }
+
 
         public override void AI() 
         { 
@@ -49,12 +52,17 @@ namespace YoguContentMod.NPCs.BossNPCs.YoguBoss
             {
                 if(jumpCount < MaxJumpsUntilLaser)
                 {
-                    Timer++;
-                    if(Timer > TimePerJump)
+                    if(npc.collideY) // q paso AAAAAAAAAAAAA ctm messi metio gol
                     {
+                        Timer++;
+                        npc.velocity.X *= 0.95f;
+                    }
+                    if(Timer > TimePerJump) 
+                    {
+                        npc.TargetClosest(true);
                         Jump();
                         jumpCount++; 
-                        Timer = 0; // reset timer
+                        Timer = 0; // reset timer  
                     }
                 }
                 else
@@ -81,13 +89,19 @@ namespace YoguContentMod.NPCs.BossNPCs.YoguBoss
             {
                 if(Timer < LaserHoldTime) 
                 {
-                    Vector2 directionToPlayer = npc.DirectionTo(Target.Center); // o
-                    Projectile.NewProjectile(npc.Center, directionToPlayer * 10, ProjectileID.EyeLaser /*placeholder*/, 40, 10f  , Main.myPlayer);
+                    if(Timer % LaserWaitTime < 1) // 
+                    { // ya reintenta
+                        npc.TargetClosest(true); 
+                        Vector2 directionToPlayer = npc.DirectionTo(Target.Center); // ya vengo voy a probar
+                        Projectile.NewProjectile(npc.Center, directionToPlayer * 10, ProjectileID.EyeLaser /*placeholder*/, 40, 10f  , Main.myPlayer);
+                    }
+
                     Timer++;                
                 }
                 else // laser attack finish 
                 {
                     Timer = 0;
+                    SecondaryTimer = 0;
                     if(laserShootCount < 2)
                     {
                         State = AIState.NPCJumping; // reset everything
@@ -120,7 +134,7 @@ namespace YoguContentMod.NPCs.BossNPCs.YoguBoss
                 else // shooting stops
                 {
                     Timer = 0;
-                    State = AIState.NPCJumping; 
+                    State = AIState.NPCJumping; // a
                     SpawnMiniSoups();        
                 }
             }    
@@ -129,10 +143,12 @@ namespace YoguContentMod.NPCs.BossNPCs.YoguBoss
         int RandomProjectileShootingTime => 120;
 
         int laserShootCount;
+        float LaserWaitTime => 10;
         float LaserHoldTime = 60;
         float LaserChargeTime => 20;
-    
-        int TimePerJump => 10;
+
+        int TimePostJumpWait => 80;    
+        int TimePerJump => 40;
         int MaxJumpsUntilLaser => 4;
         int jumpCount;
         
@@ -149,13 +165,15 @@ namespace YoguContentMod.NPCs.BossNPCs.YoguBoss
             Vector2 off = new Vector2(0, 100);
             Vector2 center = npc.Center;
             NPC.NewNPC((int)(center.X + off.X), (int)(center.Y + off.Y), ModContent.NPCType<TinySoup>());
+            NPC.NewNPC((int)(center.X - off.X), (int)(center.Y - off.Y), ModContent.NPCType<TinySoup>());
         }
         
         void Jump()
         {
-            Vector2 toPlayer = Target.MountedCenter - npc.Center;
+            npc.TargetClosest(true);
+            Vector2 toPlayer = npc.Center - Target.MountedCenter;
             npc.velocity.Y = -10; // movement to up 
-            npc.velocity.X = 10 * Math.Sign(toPlayer.X); // horizontal (x) movement
+            npc.velocity.X = 10 * -Math.Sign(toPlayer.X); // horizontal (x) movement
         } 
-    }
+    } // pos ya intenta
 }
