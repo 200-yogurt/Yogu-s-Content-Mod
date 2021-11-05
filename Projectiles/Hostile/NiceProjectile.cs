97378657d63273ee6fd8e89266e9bc23c9e5913b
200-yogurt <thekingyogurt@gmail.com>
2021-11-04 21:34:49 -0500

    projectile thingy

---
+++ Projectiles/Hostile/NiceProjectile.cs
@@ -0,0 +1,34 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace YoguContentMod
+{
+    public class NiceProjectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Nice Projectile");            
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 24;
+            Projectile.height = 28;
+            Projectile.alpha = 128;
+            Projectile.penetrate = -1;
+            Projectile.hostile = true;
+            Projectile.tileCollide = true;
+            Projectile.magic = true;
+            Projectile.ignoreWater = true;
+        }
+
+        public override void AI()
+        {
+            Projectile.rotation = Projectile.velocity.ToRotation();
+        }
+    }
+}
